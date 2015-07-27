9c467fca24e756391064b240c13b99c67c979ac3
rjsang <rjsang@gmail.com>
2015-07-28 01:06:16 +1200

    Experimenting

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,7 +2,7 @@ var app = angular.module('carson', ['ngRoute','ui.bootstrap']);
 
 app.config(function ($routeProvider, $httpProvider) {
   $routeProvider.when("/", {templateUrl: "view/report.html"});
-  $routeProvider.when("/login", {templateUrl: "login.html"});
+  $routeProvider.when("/signin", {templateUrl: "view/signin.html"});
   
   $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 });
--- src/main/resources/static/js/controllers.js
+++ src/main/resources/static/js/controllers.js
@@ -1,9 +1,65 @@
-app.controller('navController', function ($location) {
+app.controller('navController', function ($location, $http, $rootScope, $scope) {
     var ctrl = this;
+    
+    ctrl.isNew = false;
     
     ctrl.isActive = function (viewLocation) { 
         return viewLocation === $location.path();
     };
+    
+    ctrl.authenticate = function (credentials, callback) {
+
+    var headers = credentials ? {
+      authorization: "Basic "
+              + btoa(credentials.email + ":"
+                      + credentials.password)
+    } : {};
+
+    $http.get('/auth/user', {
+      headers: headers
+    }).success(function (data) {
+      if (data.name) {
+        $rootScope.authenticated = true;
+      } else {
+        $rootScope.authenticated = false;
+      }
+      callback && callback($rootScope.authenticated);
+    }).error(function () {
+      $rootScope.authenticated = false;
+      callback && callback(false);
+    });
+
+  }
+
+  ctrl.authenticate();
+
+  ctrl.credentials = {};
+
+  ctrl.login = function () {
+    ctrl.authenticate(ctrl.credentials, function (authenticated) {
+      if (authenticated) {
+        console.log("Login succeeded")
+        $location.path("/");
+        $scope.error = false;
+        $rootScope.authenticated = true;
+      } else {
+        console.log("Login failed")
+        $location.path("/login");
+        $scope.error = true;
+        $rootScope.authenticated = false;
+      }
+    })
+  };
+
+  ctrl.logout = function () {
+    $http.post('/auth/logout', {}).success(function () {
+      $rootScope.authenticated = false;
+      $location.path("/");
+    }).error(function (data) {
+      console.log("Logout failed")
+      $rootScope.authenticated = false;
+    });
+  }
 });
 
 app.controller('reportController', function (reportService) {
@@ -18,4 +74,3 @@ app.controller('reportController', function (reportService) {
         ctrl.report = response;
     });
 });
-
